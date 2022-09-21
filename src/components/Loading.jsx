9ce98caedcbc11b946bbb8ada285b00977ed86aa
Williams Hernandez <hernandw@gmail.com>
2022-09-21 11:38:59 -0300

    :sparkles: feat: add loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Loading = () => {
   return (
     <>
-      <div class="text-center">
-        <div class="spinner-border" role="status">
-          <span class="visually-hidden">Loading...</span>
+      <div className="text-center">
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
         </div>
       </div>
     </>
